b25653e17945afb03810faafecc4de23cfb5384a
Mathias Rüdiger <m.ruediger@d3media.de>
2013-08-30 17:44:28 +0200

    forgot a file m)

---
+++ llconf_run.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io"
+	"fmt"
+
+	"github.com/mruediger/llconf/parse"
+	"github.com/mruediger/llconf/promise"
+)
+
+type RunConfig struct {
+	Goal string
+	Input io.RuneReader
+	Verbose bool
+	ParseOnly bool
+}
+
+func runClient(cfg RunConfig) error {
+	fmt.Println("reading from stdin")
+	promises,err := parse.ParsePromises( cfg.Input )
+
+	if err != nil {
+		return err
+	}
+	
+	success,sout,serr := promises[cfg.Goal].Eval([]promise.Constant{})	
+	if success {
+		fmt.Println("evaluation successful\n")
+		if cfg.Verbose {
+			for _,v := range(sout) {
+				fmt.Print(v)
+			}
+		}
+	} else {
+		fmt.Println("error during evaluation\n")
+		var msgs []string
+		if cfg.Verbose {
+				msgs = append(sout,serr...)
+		} else {
+			msgs = serr
+		}
+		for _,msg := range(msgs) {
+			fmt.Print(msg)
+		}
+	}
+	
+	return nil
+}
