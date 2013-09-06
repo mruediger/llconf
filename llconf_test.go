cb1e540c511c145b94a74574931c91e9e795a3c0
Mathias Rüdiger <m.ruediger@d3media.de>
2013-09-06 11:00:39 +0200

    deleted obsolete tests

--- llconf_test.go
+++ llconf_test.go
@@ -9,15 +9,3 @@ func equals(t *testing.T, desc string, a interface{}, b interface{}) {
 		t.Errorf("%s: wantet %q, got %q", desc, a, b)
 	}
 }
-
-func TestProcessServeFlags(t *testing.T) {
-	args := []string{ "-input-folder", "/tmp", "--incomming-folder","/var"}
-	
-	cfg,err := processServeFlags("llconf", args)
-
-	if err != nil {
-		t.Errorf("%v\n", err)
-	} else {
-		equals(t, "cfg.Goal", cfg.Goal, "done")
-	}
-}
