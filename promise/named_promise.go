e63a6f902000376845376b32a85f65d2f0c07c13
Mathias Rüdiger <m.ruediger@d3media.de>
2013-09-11 15:49:48 +0200

    changed description output

--- promise/named_promise.go
+++ promise/named_promise.go
@@ -9,7 +9,7 @@ func (p NamedPromise) Desc(arguments []Constant) string {
 	if p.Promise != nil {
 		return "(" + p.Name + " " + p.Promise.Desc(arguments) + ")"
 	} else {
-		return "(" + p.Name + ">"
+		return "(" + p.Name + "<missing promise> )"
 	}
 }
 
