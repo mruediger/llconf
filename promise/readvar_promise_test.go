2534486bf2c773515d4dcc74af5aa1f3015a0074
Mathias Rüdiger <m.ruediger@d3media.de>
2013-10-23 14:22:13 +0200

    removed trailing newline

--- promise/readvar_promise_test.go
+++ promise/readvar_promise_test.go
@@ -14,8 +14,8 @@ func TestReadvarPromise(t *testing.T) {
 		Constant{"/bin/echo"},
 		Constant{"Hello World"},
 	}
-		
-		
+
+
 	exec := ExecPromise{ExecTest, arguments}
 	promise := ReadvarPromise{Constant{"test"}, exec}
 	logger := NewStdoutLogger()
@@ -23,10 +23,10 @@ func TestReadvarPromise(t *testing.T) {
 	var sout bytes.Buffer
 	logger.Stdout = &sout
 	logger.Info = &sout
-	
+
 	promise.Eval([]Constant{},&logger,&vars)
-	
-	equals(t, "Hello World\n", vars["test"])
+
+	equals(t, "Hello World", vars["test"])
 	equals(t, "/bin/echo Hello World\nHello World\n", sout.String())
 
 }
