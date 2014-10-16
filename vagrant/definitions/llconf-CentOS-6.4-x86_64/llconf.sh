12e686a7a283336660b2e1c19592202c7aa97368
Mathias Rüdiger <m.ruediger@d3media.de>
2014-10-16 11:22:07 +0200

    changed the llconf install location
    Hell... why not simply put it into the path so you can execute it simpler.
    It also removes issues you face when you want to setup your input files into
    /var/llconf

--- vagrant/definitions/llconf-CentOS-6.4-x86_64/llconf.sh
+++ vagrant/definitions/llconf-CentOS-6.4-x86_64/llconf.sh
@@ -1,3 +1,3 @@
 mkdir -p /var/llconf/bin
-wget 'https://s3-eu-west-1.amazonaws.com/llconf/llconf' -O /var/llconf/bin/llconf
-chmod +x /var/llconf/bin/llconf
+wget 'https://s3-eu-west-1.amazonaws.com/llconf/llconf' -O /usr/bin/llconf
+chmod +x /usr/bin/llconf
